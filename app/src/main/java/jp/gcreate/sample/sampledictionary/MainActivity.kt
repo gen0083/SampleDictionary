cd08f9ba3bcfae6187759fbf71a231c7ecaec347
Gen <archiherewego@gmail.com>
2018-01-18 15:48:22 +0900

    Add code to notice typo

--- app/src/main/java/jp/gcreate/sample/sampledictionary/MainActivity.kt
+++ app/src/main/java/jp/gcreate/sample/sampledictionary/MainActivity.kt
@@ -2,11 +2,16 @@ package jp.gcreate.sample.sampledictionary
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val firebase = "firebase"
+
+    (findViewById<TextView>(R.id.text)).text = firebase
   }
 }
