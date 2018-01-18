6f11d7f05c5deb24bcac42d3a74681d4720d3af9
Gen <archiherewego@gmail.com>
2018-01-18 15:40:46 +0900

    Initial commit

---
+++ app/src/main/java/jp/gcreate/sample/sampledictionary/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.gcreate.sample.sampledictionary
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
